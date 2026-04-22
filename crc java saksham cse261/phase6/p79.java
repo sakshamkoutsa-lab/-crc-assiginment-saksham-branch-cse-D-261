52d83dbd9de125fdede64ffb73ddfc47b681b225
sakshamkoutsa-lab <sakshamkoutsa@gmail.com>
2026-04-22 16:25:24 +0530

    Replace balanced parentheses check with cycle detection

--- crc java saksham cse261/phase6/p79.java
+++ crc java saksham cse261/phase6/p79.java
@@ -1,28 +1,18 @@
-import java.util.Stack;
-public class p79{
-    static boolean isBalanced(String expr){
-        Stack<Character> stack = new Stack<>();
-        for (int i=0; i<expr.length(); i++){
-            char ch=expr.charAt(i);
-            if (ch=='(' || ch=='{' || ch=='['){
-                stack.push(ch);
-            }
-            else if(ch==')' || ch=='}' || ch==']'){
-                if(stack.isEmpty()){
-                    return false;
-                }
-                char top = stack.pop();
-                if ((ch==')' && top !='(') || (ch=='}' && top !='{') || (ch == ']' && top !='[')){
-                    return false;
-                }
-            }
-        }
-        return stack.isEmpty();
-    }
+class Floydarray{
     public static void main(String[] args){
-        String test1 = "({[]})";
-        String test2 = "({[}])";
-        System.out.println(test1+" balanced? "+isBalanced(test1));
-        System.out.println(test2+" balanced? "+isBalanced(test2));
+        int[] next ={1,2,3,4,2};
+        System.out.println(hasCycle(next, 0));
+    }
+    static boolean hasCycle(int[] next, int start){
+        if(next==null||next.length==0)
+            return false;
+        int slow=start,fast=start;
+        while(fast!=-1&&next[fast]!=-1){
+            slow =next[slow];
+            fast =next[next[fast]];
+            if(slow==fast)
+                 return true;
+        }
+        return false;
     }
 }
