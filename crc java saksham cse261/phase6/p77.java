b0d99e08c2dec744aef8b9d53d00fee829720c2a
sakshamkoutsa-lab <sakshamkoutsa@gmail.com>
2026-04-22 16:24:33 +0530

    Update p77.java

--- crc java saksham cse261/phase6/p77.java
+++ crc java saksham cse261/phase6/p77.java
@@ -1,58 +1,27 @@
-public class p77{
-    int[] arr;
-    int front, rear, size, capacity;
-    p77(int cap){
-        capacity = cap;
-        arr = new int[capacity];
-        front = 0;
-        rear = -1;
-        size = 0;
-    }
-    void enqueue(int x){
-        if (size == capacity){
-            System.out.println("Queue is full");
-            return;
-        }
-        rear = (rear+1)%capacity;
-        arr[rear] = x;
-        size++;
-        System.out.println("Enqueued "+x);
-    }
-    int dequeue(){
-        if (size == 0){
-            System.out.println("Queue is empty");
-            return -1;
-        }
-        int val = arr[front];
-        front = (front+1)%capacity;
-        size--;
-        return val;
-    }
-    void display(){
-        if (size == 0){
-            System.out.println("Queue is empty");
-            return;
-        }
-        System.out.print("Queue: ");
-        int count = 0;
-        int idx = front;
-        while (count<size){
-            System.out.print(arr[idx]+" ");
-            idx = (idx+1)%capacity;
-            count++;
-        }
-        System.out.println();
-    }
+class Traprain{
     public static void main(String[] args){
-        p77 q = new p77(3);
-        q.enqueue(5);
-        q.enqueue(10);
-        q.enqueue(15);
-        q.enqueue(20);
-        q.display();
-        System.out.println("Dequeued: "+q.dequeue());
-        q.display();
-        q.enqueue(20);
-        q.display();
+        int[] arr ={0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(arr));
+    }
+    static int trap(int[] arr){
+        int l=0, r=arr.length-1;
+        int lmax=0, rmax=0, water=0;
+        while(l<r){
+            if(arr[l]<arr[r]){
+                if(arr[l]>=lmax)
+                     lmax = arr[l];
+                else
+                     water =water+(lmax-arr[l]);
+                l++;
+            } 
+            else{
+                if(arr[r]>=rmax)
+                     rmax = arr[r];
+                else 
+                    water+=(rmax-arr[r]);
+                r--;
+            }
+        }
+        return water;
     }
 }
