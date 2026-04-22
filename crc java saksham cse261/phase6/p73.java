8d1a3a61ec6e8b1d69953eb49320a7856ca90eb5
sakshamkoutsa-lab <sakshamkoutsa@gmail.com>
2026-04-22 16:21:59 +0530

    Update p73.java

--- crc java saksham cse261/phase6/p73.java
+++ crc java saksham cse261/phase6/p73.java
@@ -1,69 +1,33 @@
-class Node{
-    int data;
-    Node next;   
-    Node(int d){
-        data = d;
-        next = null;
-    }
-}
-class p73{
-    Node head;
-    void insert(int value){
-        Node newNode = new Node(value);
-        if(head == null){
-            head = newNode;
-            return;
-        }
-        Node temp = head;
-        while(temp.next != null){
-            temp = temp.next;
-        }
-        temp.next = newNode;
-    }
-    void delete(int value){
-        if(head == null){
-            System.out.println("List is empty, cannot delete");
-            return;
-        }
-        if(head.data == value){
-            head = head.next;
-            return;
-        }
-        Node temp = head;
-        while(temp.next != null && temp.next.data != value){
-            temp = temp.next;
-        }
-        if(temp.next == null){
-            System.out.println("Value "+value+" not found");
-        }
-        else{
-            temp.next = temp.next.next;
-        }
-    }
-    void display(){
-        Node temp = head;
-        if(temp == null){
-            System.out.println("List is empty");
-            return;
-        }
-        System.out.print("List: ");
-        while(temp != null){
-            System.out.print(temp.data+" -> ");
-            temp = temp.next;
-        }
-        System.out.println("null");
-    }
+class elementpos{
     public static void main(String[] args){
-        p73 list = new p73();
-        list.insert(10);
-        list.insert(20);
-        list.insert(30);
-        list.display();
-        list.delete(20);
-        list.display();
-        list.delete(10);
-        list.display();
-        list.delete(30);
-        list.display();
+        int[] nums={5,7,7,8,8,10};
+        int t = 8;
+        int[] res = search(nums, t);
+        System.out.println("["+res[0]+","+res[1]+"]");
+    }
+    static int[] search(int[] a, int t){
+        int l = 0, r =a.length-1;
+        int f =-1, ls =-1;
+        while(l<=r){
+            int m = l+(r-l)/2;
+            if(a[m]>=t)
+                 r =m-1;
+            else 
+                l =m+1;
+            if (a[m]==t) 
+                f = m;
+        }
+        l = 0; r=a.length-1;
+        while(l<=r){
+            int m =l+(r-l)/2;
+            if (a[m]<=t)
+                 l = m+1;
+            else 
+                 r = m-1;
+            if(a[m]==t) 
+                 ls =m;
+        }
+        System.out.println(new int[]{f, ls});
+        return new int[]{f, ls};
     }
 }
